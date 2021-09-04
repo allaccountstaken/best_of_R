72efee3ca2bb93cbe7bb41fca01a80ab7ff1ef10
Dima Mikhaylow <dmik@vt.edu>
2021-09-03 23:07:28 -0400

    M2 - started the guided question set2

--- M2/Data_visualization_start.R
+++ M2/Data_visualization_start.R
@@ -60,3 +60,84 @@ ggplot(Data, aes(x=Age))+
 
 ggplot(Data, aes(x=Age))+
   geom_density()
+
+
+# Bivariate data visualization 
+library(gapminder)
+gapminder[1:5, ]
+
+Data<-gapminder%>%
+  filter(year==2007)
+
+ggplot(Data, aes(x=continent, y=lifeExp))+
+  geom_boxplot(fill="Blue")+
+  labs(x="Continent", y="Life Exp",
+       title="Dist of Life Expectancies by Continent")
+
+       
+ggplot(gapminder, aes(x=continent, y=lifeExp, fill=as.factor(year)))+
+  geom_boxplot()+
+  labs(x="Continent", y="Life Exp",
+       title="Dist of Life Expectancies by Continent & Year")
+
+Data <- Data%>%
+  mutate(expectancy=ifelse(lifeExp<70, "Low", "High"))
+
+mytab2 <- table(Data$continent, Data$expectancy)
+mytab2
+
+prop.table(mytab2, 1)       
+
+round(prop.table(mytab2, 1) * 100, 2)
+
+
+ggplot(Data, aes(x=continent, fill=expectancy))+
+  geom_bar(position="stack")+
+  labs(x="Continent", y="Count", title="Life Expectancies by Continent")
+
+
+ggplot(Data, aes(x=continent, fill=expectancy))+
+  geom_bar(position="dodge")
+
+
+ggplot(Data, aes(x=continent, fill=expectancy))+
+  geom_bar(position="fill")+
+  labs(x="Continent", y="Proportion",
+       title="Proportion of Life Expectancies by Continent")
+
+
+ggplot(Data, aes(x=gdpPercap, y=lifeExp))+
+  geom_point(alpha=0.2)
+
+# Multivariate
+Data <- gapminder%>%
+  mutate(expectancy=ifelse(lifeExp<70, "Low", "High"))%>%
+  filter(year==2007)
+
+Data.all <- gapminder%>%
+  mutate(expectancy=ifelse(lifeExp<70, "Low", "High"))
+
+
+ggplot(Data, aes(x=continent, fill=expectancy))+
+  geom_bar(position = "fill")
+
+ggplot(Data.all, aes(x=continent, fill=expectancy))+
+  geom_bar(postion = "fill") +
+  facet_wrap(~ year)
+
+
+ggplot(Data, aes(x=gdpPercap, y=lifeExp, size=pop))+
+  geom_point()+
+  scale_size(range = c (0.1, 12))
+
+
+ggplot(Data, aes(x=gdpPercap, y=lifeExp, size=pop, color=continent))+
+  geom_point()+
+  scale_size(range = c (0.1, 12))
+
+
+
+ggplot(Data, aes(x=gdpPercap, y=lifeExp, size=pop, fill=continent))+
+  geom_point(shape=21, alpha=0.5)+
+  scale_size(range = c(0.1,12))+
+  labs(x="GDP", y="Life Exp", title="Scatterplot of Life Exp against GDP")
